c962b1ec9c7579f942f5dbb3075619ff8142fb82
dwivedi6 <90080512+dwivedi6@users.noreply.github.com>
2021-09-06 13:12:58 +0530

    Add files via upload

---
+++ mt19aie323-helloiitj.py
@@ -0,0 +1,9 @@
+## Who has written =MT19AIE323, 
+# What is the function = It will print Hello IITj 5 times in every 2 seconds
+
+from time import sleep
+i=0;
+while (i<5): # loop till i is less than 5
+    print("Hello IITj") # print statement
+    sleep(2) # sleep for 2 seconds
+    i=i+1 # country increment
